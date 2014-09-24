b1045976dea4c2ded8340bb136f312c517fd80fc
Severyn Kozak <severyn.kozak@gmail.com>
2014-09-23 21:23:10 -0400

    Add custom excerpt filter.
    _plugins/excerpt.rb, _includes/post_summary.html
    	-Add an excerpt filter that outputs *only* the first paragraph
    	of a post, without leading headers or other markup.

---
+++ _plugins/excerpt.rb
@@ -0,0 +1,15 @@
+# A Liquid filter to extract only the first paragraph of a blog post, without
+# any leading headers or other markup.
+
+require 'nokogiri'
+
+module Jekyll
+	module PostExcerpt
+		def excerpt(html)
+			tree = Nokogiri::HTML(html)
+			tree.css('body > p')[0].text
+		end
+	end
+end
+
+Liquid::Template.register_filter(Jekyll::PostExcerpt)
